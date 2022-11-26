8f183997b24f86bf34a511a09c34aef53506f31c
nawaz990 <yasirnawaz990@gmail.com>
2022-11-26 14:34:21 +0530

    mongo script

--- catalogue.sh
+++ catalogue.sh
@@ -13,5 +13,13 @@ mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
 
+sed -i -e 's/MONGO_DNSNAME/mongodb.deveopsb69.online/' systemd.service
+
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl start catalogue
+systemctl enable catalogue
+
+
 
 
