798f6d1be4e52a817feadef3750d3d725f7b5a73
nawaz990 <yasirnawaz990@gmail.com>
2022-11-26 15:13:08 +0530

    user script

--- user.sh
+++ user.sh
@@ -10,8 +10,9 @@ mv user-main user
 cd /home/roboshop/user
 npm install
 
+sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/MONGO_ENDPOINT/mongo.devopsb69.online/' systemd.service
 
-# mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
-# systemctl daemon-reload
-# systemctl start user
-# systemctl enable user
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl start user
+systemctl enable user
