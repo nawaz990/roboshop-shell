aca5fa18ab0176d361dae07080bf040651ce4e0b
nawaz990 <yasirnawaz990@gmail.com>
2022-11-29 01:33:17 +0530

    Mysql script

--- mysql.sh
+++ mysql.sh
@@ -12,3 +12,4 @@ then
   echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1';" > /tmp/root-pass-sql
   DEFAULT_PASSWORD=$(grep 'A temporary password'  /var/log/mysqld.log | awk '{print $NF}')
   cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
+fi
