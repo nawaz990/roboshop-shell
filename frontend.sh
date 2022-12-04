35af239f204ffa1f39cfaa4fa2a6b325a206c7ea
nawaz990 <yasirnawaz990@gmail.com>
2022-12-05 03:21:53 +0530

    Updated roboshop configuration for catalogue, user, cart, shipping, & payment

--- frontend.sh
+++ frontend.sh
@@ -17,7 +17,7 @@ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 STAT $?
 
 PRINT "Update Roboshop Configuration"
-sed -i -e '/catalogue/ s/localhost/catalogue-dev.devopsn69.online/' -e '/user/ s/localhost/user-dev.devopsn69.online/' -e '/cart/ s/localhost/cart-dev.devopsn69.online/' -e '/shipping/ s/localhost/shipping-dev.devopsn69.online/' -e '/payment/ s/localhost/payment-dev.devopsn69.online/' /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/dev-catalogue.devopsn69.online/' -e '/user/ s/localhost/dev-user.devopsn69.online/' -e '/cart/ s/localhost/dev-cart.devopsn69.online/' -e '/shipping/ s/localhost/dev-shipping.devopsn69.online/' -e '/payment/ s/localhost/dev-payment.devopsn69.online/' /etc/nginx/default.d/roboshop.conf
 STAT $?
 
 PRINT "Enable Nginx Service"
